15faf8274c907977d698baa4fbed7898149976ff
flyswatter <somniac@me.com>
2013-05-14 23:37:50 -0700

    Committing before editing for the night.

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
+//Randomize & cycle tagline:
 var taglines = [
-	//"A man of many hats.",
 	"A specimen of human life.",
 	"Certainy a person.",
+	"Probably a person.",
+	"Can anyone ever really define person?",
+	"A person.  There, I said it.",
 	"A gloriously humble man.",
 	"A man fueled by burritos, preferably.",
 	'A website you can "trust".',
 	"Apparently capable.",
-	"He thinks he thinks.",
+	"thinks he thinks.",
 	"Still kicking, despite things.",
-	"The man, the website."
+	"The man, the website.",
+	'"Meh."',
+	"Precisely vague.",
+	"Deceptively honest.",
+	"is what he ate.",
+	"may be immortal.",
+	"has a variety of reputations."
 	]
+var tagNumber = Math.round((Math.random()*100))%taglines.length;
 
-var randNum = Math.round((Math.random()*100))%taglines.length;
-console.log("Chose "+randNum)
-$('#tagline').text(taglines[randNum]);
+function readyFunction(){
+	$('.tagline').text(taglines[tagNumber]);
+	var newTagline = window.setInterval(function(){
+		if(++tagNumber>=taglines.length){tagNumber=0;}
+		// console.log("Starting transition")
+
+    	// $('.tagline').text(taglines[tagNumber]);
+		$('.tagline').fadeOut('slow',
+	        function(){
+	        	$('.tagline').text(taglines[tagNumber]);
+			    // console.log("Completed")
+				$('.tagline').fadeIn('slow');
+			})
+	}, 3500);
+}
